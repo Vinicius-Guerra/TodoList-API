72de3634289b2e0f3afb965823def7de44841750
Vinicius Guerra <vini.fms@hotmail.com>
2024-03-04 13:52:07 -0300

    Helmet

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import "express-async-errors";
 import express, { json } from "express";
+import helmet from "helmet";
 import { todoRoutes } from "./routes/todo.routes";
 import { HandleErrors } from "./errors/handleErrors";
 
 export const app = express();
+
+app.use(helmet());
 
 app.use(json());
 
